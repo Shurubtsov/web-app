568ec296fb562cbfbcf3e0a4faf67819d2b8c100
Danila <qlyouqwa4ka@gmail.com>
2022-05-23 01:13:49 +0300

    add sql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 
 	"dshurubtsov.com/snippetbox/cmd/config"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	// configuration
 	addr := flag.String("addr", ":4000", "Net address HTTP")
+	dsn := flag.String("dsn", "web:0907@/snippetbox?parseTime=true", "name MySQL data source")
 	flag.Parse()
 
 	// configure new loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  // info
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // errors
+
+	// configure database connection
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// init application object
 	app := &config.Application{
@@ -32,6 +42,17 @@ func main() {
 	}
 
 	infoLog.Printf("start server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
